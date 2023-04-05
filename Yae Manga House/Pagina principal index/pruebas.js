1b789742ea474b833b20eec0a2d61f1a5bb13e50
xSneyder <dsmenesesnarvaez@gmail.com>
2023-04-05 19:31:53 +0200

    Pagina toatalmente limpia

---
+++ Yae Manga House/Pagina principal index/pruebas.js
@@ -0,0 +1,9 @@
+ // Cambia solo el código debajo de esta línea
+ const myName = "Deivy";
+ const myStr = "My name is " + myName  + " and I am well";
+ 
+ 
+ document.write(myStr);
+
+ 
+ console.log(myStr);
